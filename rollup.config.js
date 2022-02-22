b9318770aed18b6d66331ed39d40572e80cae4ad
Jon-Carlos Rivera <jon.carlos.rivera@gmail.com>
2022-02-21 22:54:39 -0700

    Updated rollup to use config

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import uglify from 'rollup-plugin-uglify';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import nodePolyfill from 'rollup-plugin-polyfill-node';
+
+export default {
+  input: 'src/ihop.js',
+  output: [
+    {
+      file: 'dist/ihop.js',
+      format: 'iife',
+      name: 'IHOP',
+    }, {
+      file: 'dist/ihop.min.js',
+      format: 'iife',
+      name: 'IHOP',
+      plugins: [uglify],
+    }
+  ],
+  plugins: [
+    nodePolyfill(),
+    nodeResolve(),
+    commonjs({
+      exclude: ["src/**"],
+      include: ["node_modules/**"],
+    }),
+  ],
+  //external: ['same-origin']
+};
